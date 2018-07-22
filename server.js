0e20641b4ea0ff1eb29fbb2d59bfe52b988bf5a4
farha-neu <farhajw@gmail.com>
2018-07-22 14:22:01 -0700

    backend connected

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,6 +26,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhost/nytreact");
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
