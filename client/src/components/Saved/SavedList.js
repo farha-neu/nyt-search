f952cb57a55bfcf6676fd19fb64092aa57a6071a
farha-neu <farhajw@gmail.com>
2018-07-24 18:37:21 -0700

    merge

--- client/src/components/Saved/SavedList.js
+++ client/src/components/Saved/SavedList.js
@@ -1,23 +1,33 @@
 import React from "react";
 import Moment from 'react-moment';
+import NoteBox from './NoteBox';
 import "../Home/Home.css";
 import "./Saved.css";
 import ReactTooltip from 'react-tooltip';
 
-const SavedList =(props)=>
-        (
-            <div className="col-md-12 news" key={props.id}>
+class SavedList extends React.Component{
+    state={
+        clicked : false
+    }
+   showNote=()=>{
+       var noteClicked = this.state.clicked;
+       return(this.setState({clicked:!noteClicked}));
+    }
+       
+    render(){
+        return (
+            <div className="col-md-12 news" key={this.props.id}>
                     <div className="row">
                         <div className="col-md-8">
-                            <p className="headline"><a href={props.url}>{props.title}</a></p>
-                            <p className="snippet">{props.snippet}</p>
-                            <div className="byline"><span>{props.byline}</span><i className="fas fa-circle ml-1 mr-1"></i>
-                            <span> Published: <Moment format="YYYY/MM/DD h:mm:ssa">{props.date}</Moment></span></div>
-                            <p className="mt-1"> Date Saved: <Moment format="YYYY/MM/DD h:mm:ssa">{props.dateSaved}</Moment></p>
+                            <p className="headline"><a href={this.props.url}>{this.props.title}</a></p>
+                            <p className="snippet">{this.props.snippet}</p>
+                            <div className="byline"><span>{this.props.byline}</span><i className="fas fa-circle ml-1 mr-1"></i>
+                            <span> Published: <Moment format="YYYY/MM/DD h:mm:ssa">{this.props.date}</Moment></span></div>
+                            <p className="mt-1"> Date Saved: <Moment format="YYYY/MM/DD h:mm:ssa">{this.props.dateSaved}</Moment></p>
                         </div>
                         <div className="col-md-2">
-                            {props.image?
-                            <img className="img-fluid news-img" alt='news-pic' src={`https://static01.nyt.com/${props.image}`} />
+                            {this.props.image?
+                            <img className="img-fluid news-img" alt='news-pic' src={`https://static01.nyt.com/${this.props.image}`} />
                             :""} 
                         </div>
                         <div className="col-md-2">
@@ -29,6 +39,7 @@ const SavedList =(props)=>
                     </div>
            </div>
         )
-    
+    }
+}     
 
 export default SavedList;
